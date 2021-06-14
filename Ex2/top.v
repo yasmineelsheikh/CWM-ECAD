e20c7cb4d525f147d2c707019ebd8203abe17ecc
Cloud User <centos@ip-172-31-3-169.eu-west-2.compute.internal>
2021-06-14 14:41:29 +0000

    Solution to Ex2

--- Ex2/top.v
+++ Ex2/top.v
@@ -22,11 +22,17 @@
 
 module doorbell(
     //Todo: define inputs here
+      input a,
+      input b,
+      input sel,
+      output out
     );
     
     //Todo: define registers and wires here
+      wire out;
 
 
-    //Todo: define your logic here                 
+    //Todo: define your logic here    
+      assign #5 out = sel? b : a;         
       
 endmodule
