03699c1eff0ad17cdfbfe2a392752119ab0b7c96
Cloud User <centos@ip-172-31-3-169.eu-west-2.compute.internal>
2021-06-15 12:02:26 +0000

    Solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Yasmine Elsheikh
+// Date: 15/06/21
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,31 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module LED (
+//Ports
+input clk,
+input rst,
+input button,
+output reg [2:0] colour);
+
+//registers and wires
+always @(posedge clk)
+
+//user logic
+begin
+
+if (rst == 1)
+colour = 3'b0;
+
+else begin
+if (button == 0)
+colour <= #1 ( (colour == 3'b000) | (colour == 3'b111)) ? 3'b001 : colour;
+else
+colour <= #1 ( (colour < 3'b110)) ? (colour + 3'b001): (3'b001);
+
+end 
+
+end
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,81 @@
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module top_tb(
+   );
+
+//Parameters
+parameter CLK_PERIOD = 10;
+reg clk;
+reg rst;
+reg button;
+reg err;
+reg [2:0] colour_1;
+wire[2:0] colour;
+
+//clock generation
+initial 
+   begin
+     clk = 1'b0;
+     forever 
+       #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//user logic
+initial begin
+     rst=1;
+     button=0;
+     err=0;
+     colour_1=3'b111;
+     #(CLK_PERIOD*10);
+ if ( !(colour == 3'b0) || (colour == 3'b1))
+ begin
+ $display("***Test failed!");
+ err=1;
+ end
+ rst=0;
+ if ( colour != 3'b001)
+ begin
+ $display("***Test failed! rst=1, colour=%h", colour);
+ end
+
+ #(CLK_PERIOD*10);
+ if (colour != 0)
+ begin
+ $display("***Test failed! rst=1, colour=%h", colour);
+ err=1;
+ end 
+ button=1;
+    forever begin 
+      #(CLK_PERIOD)
+ if (button == 1)
+ colour_1 = colour_1<3'b110 ? colour_1 + 3'b001: 3'b001;
+ if (colour_1 != colour)
+ begin
+ $display("***Test failed! rst=0, colour_1=%h, colour=%h", colour_1, colour);
+ err = 1;
+ end
+ end
+
+ end
+
+//Instantiate counter module
+LED top(
+.rst (rst),
+.clk (clk),
+.button (button),
+.colour (colour)
+);
+
+//finish test, check for success
+initial begin
+     #50;
+     if (err==0)
+       $display("***Test Passed!");
+      $finish;
+    end
+endmodule
+
+    
