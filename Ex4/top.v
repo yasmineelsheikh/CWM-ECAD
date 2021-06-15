c41daf3e104d754bfc865556c9199817e6661021
Cloud User <centos@ip-172-31-3-169.eu-west-2.compute.internal>
2021-06-15 13:34:59 +0000

    Solution to ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -15,31 +15,32 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
-`timescale 1ns/100ps
+`timescale 1ns / 100ps
 
 module LED (
 //Ports
 input clk,
 input rst,
 input button,
-output reg [2:0] colour);
+output reg [2:0] colour
+);
 
 //registers and wires
-always @(posedge clk)
+always @ (posedge clk)
 
 //user logic
 begin
 
 if (rst == 1)
-colour = 3'b0;
+   colour = 3'b0;
 
 else begin
-if (button == 0)
-colour <= #1 ( (colour == 3'b000) | (colour == 3'b111)) ? 3'b001 : colour;
-else
-colour <= #1 ( (colour < 3'b110)) ? (colour + 3'b001): (3'b001);
+    if (button == 0)
+    colour <= ((colour == 3'b000) || (colour == 3'b111)) ? 3'b001: colour;
+    else
+    colour <= (colour < 3'b110)? colour + 3'b001: 3'b001;
 
-end 
+    end 
 
-end
+  end
 endmodule
